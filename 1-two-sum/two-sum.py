e69e109cf7c14e00e1eab17508280afe039cecfa
dvm9999 <32097658+dvm9999@users.noreply.github.com>
2026-03-05 09:24:57 +0530

    Time: 1 ms (60.25%) | Memory: 20.3 MB (63.48%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
+        for idx, num in enumerate(nums):
+            if target - num in lookup:
+                return [lookup[target - num], idx]
+            lookup[num] = idx
+    
