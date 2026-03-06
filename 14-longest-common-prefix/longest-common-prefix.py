0ebc448dca3368a26833699e4f3ad04d63688e23
dvm9999 <32097658+dvm9999@users.noreply.github.com>
2026-03-06 21:30:52 +0530

    Time: 2 ms (25.64%) | Memory: 19.2 MB (90.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        shorteststr =  min(strs, key = len)
+        shorteststr =  min(strs)
         for i, c in enumerate(shorteststr):
             for str in strs:
                 if str[i] != c:
