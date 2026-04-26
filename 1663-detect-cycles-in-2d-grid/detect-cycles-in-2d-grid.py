e13e5aa708b634fc55fd65c6088782bcd9e142b5
dvm9999 <32097658+dvm9999@users.noreply.github.com>
2026-04-26 06:12:05 +0530

    Time: 407 ms (55.15%) | Memory: 69.7 MB (88.01%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -7,7 +7,7 @@ class Solution:
             while(q):
                 x, y, px, py = q.popleft()
                 for nx, ny in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
-                    if 0 <=nx < row and 0 <=ny < col and grid[nx][ny] == c:
+                    if 0 <= nx < row and 0 <= ny < col and grid[nx][ny] == c:
                         if (nx, ny) in visited:
                             if (nx, ny) != (px, py):
                                 return True
