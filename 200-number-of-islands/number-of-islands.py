ca726f25928212a3b80bb2bd10b5a8c133b41996
dvm9999 <32097658+dvm9999@users.noreply.github.com>
2026-05-07 10:22:26 +0530

    Time: 231 ms (89.76%) | Memory: 21.8 MB (33.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,7 +5,7 @@ class Solution:
         def dfs(i, j):
             if grid[i][j] == "1":
                 grid[i][j] = '0'
-            for x, y in [(i + 1, j), ( i - 1, j), (i, j + 1), (i, j -1)]:
+            for x, y in [(i + 1, j), ( i - 1, j), (i, j + 1), (i, j - 1)]:
                 if 0 <= x < row and 0 <= y < col and grid[x][y] == "1":
                     dfs(x, y)
         noOfIslands  = 0
