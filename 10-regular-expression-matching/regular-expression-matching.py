bfdbe166fb27b64e5fe0a49e349240c66e44eda9
dvm9999 <32097658+dvm9999@users.noreply.github.com>
2026-05-21 06:44:18 +0530

    Time: 7 ms (60.60%) | Memory: 20.5 MB (17.78%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -7,10 +7,10 @@ class Solution:
             if j == len(p):
                 memo[i, j]  = i == len(s)
             else:
-                first_match = i < len(s) and p[j] in {s[i], '.'}
+                first_match = i < len(s) and p[j] in s[i] + '.'
                 if j + 1 < len(p) and p[j + 1] == '*':
                     memo[i,j]= dfs(i, j + 2) or first_match and dfs(i + 1, j)
                 else:
-                    memo[i,j]= first_match and dfs(i + 1, j + 1)                
+                    memo[i, j]= first_match and dfs(i + 1, j + 1)                
             return memo[i, j]
         return dfs(0, 0)
